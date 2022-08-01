ebb2c5bc676720d089e37073d904ba75c3794735
Daniel Roe <daniel@roe.dev>
2022-08-01 16:49:02 +0100

    chore: try inline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,20 @@ import { execaCommand } from 'execa'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
   hooks: {
-    "nitro:init"(nitro) {
+    'nitro:init'(nitro) {
       nitro.hooks.hook('compiled', async () => {
-        await execaCommand('tar -cvzf dist/app.tgz --exclude node_modules --exclude .git --exclude dist/app.tgz .')
+        await execaCommand(
+          'tar -cvzf dist/app.tgz --exclude node_modules --exclude .git --exclude dist/app.tgz .'
+        )
       })
-    }
+    },
   },
 })
